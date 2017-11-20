dedbd83525f92c3bdaf8e8aca3350ca2ab85211f
sunwenwu <sunwenwu@ucfgroup.com>
2017-11-20 17:36:35 +0800

    修改参数

--- dev-demo/src/main/java/tempTest/newAbstracClass/Test.java
+++ dev-demo/src/main/java/tempTest/newAbstracClass/Test.java
@@ -10,7 +10,7 @@ public class Test {
 
     //new 一个抽象类，重写抽象方法后，再调用
     public static void main(String[] args) {
-        new AbTest("sww","man"){
+        new AbTest("swwwenwu","man"){
             @Override
             public void handle() {
                 System.out.println(this.getName()+" "+this.getSex());
