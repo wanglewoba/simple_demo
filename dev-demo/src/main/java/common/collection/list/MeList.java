481c7b16405b12496e2dec7e07c2e5ddb9e3497a
sunwenwu <sunwenwu@imeiyebang.com>
2018-03-26 15:47:39 +0800

    测试用例

--- dev-demo/src/main/java/common/collection/list/MeList.java
+++ dev-demo/src/main/java/common/collection/list/MeList.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class MeList {
 
     public static void main(String[] args) {
+        List<List<AA>> resultList = new ArrayList<>();
 
         List<AA> tl = new ArrayList<>();
         AA aa;
@@ -18,8 +19,26 @@ public class MeList {
             aa.setName(i+" "+i);
             tl.add(aa);
         }
-        System.out.println(tl);
+        System.out.println("测试数据"+tl);
+
+        List<AA> smallList = null;
+        int k=3;//每行显示数
+        for (int i=0;i<tl.size();i++){
+            if(i%k == 0){
+                smallList = new ArrayList<>();
+                resultList.add(smallList);
+            }
+            smallList.add(tl.get(i));
+        }
+        for(int j=0;j<resultList.size();j++){
+            System.out.println("第"+(j+1)+" 返回数据 "+resultList.get(j));
+        }
     }
+
+
+
+
+
 }
 class AA{
     private Integer id;
