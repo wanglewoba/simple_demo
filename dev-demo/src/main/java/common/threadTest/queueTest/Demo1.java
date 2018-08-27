41e8c9acf8a73a5ce14b8b6b22d7eeb2819af778
sunwenwu <sunwenwu@imeiyebang.com>
2018-08-27 16:48:44 +0800

    修改统计多线程统计方式

--- dev-demo/src/main/java/common/threadTest/queueTest/Demo1.java
+++ dev-demo/src/main/java/common/threadTest/queueTest/Demo1.java
@@ -2,6 +2,7 @@ package common.threadTest.queueTest;
 
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.CountDownLatch;
 
 
 /**
@@ -11,7 +12,7 @@ public class Demo1 {
 
     static Queue<String> queue = new ConcurrentLinkedQueue<>();
     static {
-        for (int i=0;i<100000;i++){
+        for (int i=0;i<1000000;i++){
             queue.add("产品："+i);
         }
     }
@@ -19,14 +20,17 @@ public class Demo1 {
     public static void main(String[] args) {
 
         long start = System.currentTimeMillis();
+        int length = 20;
+        CountDownLatch cdl = new CountDownLatch(length);
 
-        for(int j=0;j<20;j++){
+        for(int j=0;j<length;j++){
             new Thread("线程"+j){
                 @Override
                 public void run() {
                     while (true){
                         String poll = queue.poll();
                         if(poll == null){
+                            cdl.countDown();
                             break;
                         }else {
                             System.out.println(Thread.currentThread().getName()+":"+poll);
@@ -35,14 +39,17 @@ public class Demo1 {
                 }
             }.start();
         }
-        long end = 0;
-        while(true){
-            if(queue.poll() == null){
-                end = System.currentTimeMillis();
-                break;
-            }
+
+        try {
+            cdl.await();//等待所有线程执行完
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        long end = System.currentTimeMillis();
 
         System.out.println("===========执行时间："+(end-start));
+        //===========执行时间：713、7533
+
+
     }
 }
--- dev-demo/src/main/java/common/threadTest/queueTest/Demo2.java
+++ dev-demo/src/main/java/common/threadTest/queueTest/Demo2.java
@@ -2,6 +2,7 @@ package common.threadTest.queueTest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * Created by sunwenwu on 2018/8/27.
@@ -11,7 +12,7 @@ public class Demo2 {
     static List<String> list = new ArrayList<>();
 
     static {
-        for (int i=0;i<100000;i++){
+        for (int i=0;i<1000000;i++){
             list.add("产品："+i);
         }
     }
@@ -19,14 +20,17 @@ public class Demo2 {
     public static void main(String[] args) {
 
         long start = System.currentTimeMillis();
+        int length = 20;
+        CountDownLatch cdl = new CountDownLatch(length);
 
-        for(int j=0;j<20;j++){
+        for(int j=0;j<length;j++){
             new Thread("线程"+j){
                 @Override
                 public void run() {
                     while (true){
                         synchronized (list){
                             if(list.size()<=0){
+                                cdl.countDown();
                                 break;
                             }else {
                                 System.out.println(Thread.currentThread().getName()+":"+list.remove(0));
@@ -36,13 +40,15 @@ public class Demo2 {
                 }
             }.start();
         }
-        long end = 0;
-        while(true){
-            if(list.size()<=0){
-                end = System.currentTimeMillis();
-                break;
-            }
+
+        try {
+            cdl.await();//等待所有线程执行完
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        long end = System.currentTimeMillis();
         System.out.println("===========执行时间："+(end-start));
+        //===========执行时间：813、6722
+
     }
 }
